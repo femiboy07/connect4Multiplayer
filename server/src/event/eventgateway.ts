8e33b7af6069c395ad6d64076c134e97642720eb
femiooboy <okonoluwaferanmi@gmail.com>
2024-10-07 04:18:07 +0100

    delete game when player reloads

--- server/src/event/eventgateway.ts
+++ server/src/event/eventgateway.ts
@@ -274,24 +274,21 @@ export class EventsGateway
 
   handleDisconnect(@ConnectedSocket() client: Socket) {
     console.log(client);
-    // client.on('disconnecting', () => {
-    // for (const room of client.rooms) {
-    this.gameManager.handleDisconnect(client);
-    this.gameManager.users.filter((u) => u.socket.id !== client.id);
-    client.emit('user_has_left', client.id);
-    const games = this.gameManager.games.find(
-      (game) =>
-        game.player1.socket.id === client.id ||
-        game.player2.socket.id === client.id,
-    );
+    client.on('disconnecting', () => {
+      this.gameManager.handleDisconnect(client);
+      this.gameManager.users.filter((u) => u.socket.id !== client.id);
+      client.emit('user_has_left', client.id);
+      const games = this.gameManager.games.find(
+        (game) =>
+          game.player1.socket.id === client.id ||
+          game.player2.socket.id === client.id,
+      );
 
-    const gameIndex = this.gameManager.games.indexOf(games);
+      const gameIndex = this.gameManager.games.indexOf(games);
 
-    if (gameIndex !== -1) {
-      this.gameManager.games.splice(gameIndex, 1);
-    }
-
-    // }
-    // });
+      if (gameIndex !== -1) {
+        this.gameManager.games.splice(gameIndex, 1);
+      }
+    });
   }
 }
