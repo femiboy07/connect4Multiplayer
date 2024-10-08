0aee188fd61a1287c514c15812eba56d73680034
femiooboy <okonoluwaferanmi@gmail.com>
2024-10-08 04:40:24 +0100

    update

--- client/src/SocketContext.ts
+++ client/src/SocketContext.ts
@@ -10,7 +10,7 @@ export const sockerServerUrl = port.replace(/^http/, protocol);
 
 // Create a context for the socket
 
-export const socket =io(sockerServerUrl,{
+export const socket =io("https://serverconnect-kxhn.onrender.com/",{
     transports:['websocket']
 })
 
