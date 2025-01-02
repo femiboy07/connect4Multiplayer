e21fb3e5adfa9a04c2d658c46d94da11440d44dc
femiooboy <okonoluwaferanmi@gmail.com>
2025-01-02 21:55:31 +0100

    tweaked-status

--- client/src/SocketContext.ts
+++ client/src/SocketContext.ts
@@ -1,23 +1,21 @@
-import React, { createContext,useContext} from 'react';
+import React, { createContext, useContext } from 'react';
 import { io, Socket } from 'socket.io-client';
 
-// Replace with your server URL
-const port = process.env.REACT_APP_WEBSOCKET_URL || "http://localhost:3000";
-const protocol = port.startsWith('https') ? 'https' : 'ws';
-export const sockerServerUrl = port.replace(/^http/, protocol);
-// export const sockectSeverUrl=port;
+// Get WebSocket URL
+const port = process.env.NODE_ENV === 'production' ? "https://multiplayerconnect4.onrender.com" : "http://localhost:3000";
+const protocol = port.startsWith('https') ? 'wss' : 'ws';
+export const sockerServerUrl = port.replace(/^http(s?):\/\//, `${protocol}://`);
 
+// Create the WebSocket instance
+export const socket = io(sockerServerUrl, {
+    transports: ['websocket'], // Test removing this if issues persist
+    reconnection: true // Enable automatic reconnection
+});
 
-// Create a context for the socket
+// Create Socket Context
+export const SocketContext = createContext<Socket | null>(socket);
 
-export const socket =io(sockerServerUrl,{
-    transports:['websocket']
-})
-
- export const SocketContext = createContext<Socket | null>(socket);
-
-
-export function useSocket():Socket {
+export function useSocket(): Socket {
     const context = useContext(SocketContext);
     if (!context) {
         throw new Error('useSocket must be used within a SocketProvider');
