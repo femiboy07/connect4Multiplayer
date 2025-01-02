21bed709c2f7b1abde965f646bba1816b1573658
femiooboy <okonoluwaferanmi@gmail.com>
2025-01-02 20:13:18 +0100

    tweaked-status

--- client/src/SocketContext.ts
+++ client/src/SocketContext.ts
@@ -3,7 +3,7 @@ import { io, Socket } from 'socket.io-client';
 
 // Replace with your server URL
 const port = process.env.REACT_APP_WEBSOCKET_URL || "http://localhost:3000";
-const protocol = port.startsWith('https') ? 'wss' : 'ws';
+const protocol = port.startsWith('https') ? 'https' : 'ws';
 export const sockerServerUrl = port.replace(/^http/, protocol);
 // export const sockectSeverUrl=port;
 
